fb16a520cf8dc6d109d91ea30bfb0cf2c9572364
kaderabdul <kaderabdul7980@gmail.com>
2022-02-26 18:19:27 +0600

    fetch data from server

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
 const app = express()
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
 const port = 5000
 
+// use middleware and parse data
+app.use(cors());
+app.use(express.json());
 
 const uri = "mongodb+srv://apartment:sai1eFqglF3FEUoL@cluster0.vxcvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+// const uri = `mongodb+srv://apartment:sai1eFqglF3FEUoL@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// 
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("apartment");
+      const scheduleCollection = database.collection("schedule");
+
+    // collect data from body and create post api to insert data for schedule
+    app.post('/schedules', async (req, res) => {
+        const schedule = req.body;
+        const result = await scheduleCollection.insertOne(schedule);
+        console.log(result);
+        res.json(result)
+    })
+    
+    // fetch data
+    app.get('/schedules', async (req, res) => {
+        const query = {}
+        const cursor = scheduleCollection.find(query);
+        const schedules = await cursor.toArray();
+        res.send(schedules);
+    })
+  
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
 
 
 app.listen(port, () => {
