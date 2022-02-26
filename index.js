854be0d117a0e6cd904c3b3572c32d5b3ba6a49f
kaderabdul <kaderabdul7980@gmail.com>
2022-02-26 21:55:02 +0600

    user post api create when user'll create new account

--- index.js
+++ index.js
@@ -19,21 +19,32 @@ async function run() {
       await client.connect();
       const database = client.db("apartment");
       const scheduleCollection = database.collection("schedule");
+      const userCollection = database.collection("user");
 
     // collect data from body and create post api to insert data for schedule
     app.post('/schedules', async (req, res) => {
         const schedule = req.body;
         const result = await scheduleCollection.insertOne(schedule);
-        console.log(result);
+        // console.log(result);
         res.json(result)
     })
     
     // fetch data
     app.get('/schedules', async (req, res) => {
-        const query = {}
+        const email = req.query.email;
+        const query = {email: email}
+        console.log(query)
         const cursor = scheduleCollection.find(query);
         const schedules = await cursor.toArray();
-        res.send(schedules);
+        res.json(schedules);
+    })
+
+    // insert data for user it'll called from useFirebase under userRegistration function
+    app.post('/users', async (req, res) => {
+    const user = req.body;
+    const result = await userCollection.insertOne(user);
+    // console.log(result);
+    res.json(result)
     })
   
     } finally {
