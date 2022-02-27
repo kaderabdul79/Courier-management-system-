792e4547d184121caa64847c37783e303625b227
kaderabdul <kaderabdul7980@gmail.com>
2022-02-27 16:01:15 +0600

    add role when admin will make someone admin

--- index.js
+++ index.js
@@ -56,6 +56,15 @@ async function run() {
         const result = await usersCollection.updateOne(filter, updateDoc, options);
         res.json(result);
     });
+
+    // add role when admin will make someone admin
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: {role: 'admin'} };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+  });
   
     } finally {
     //   await client.close();
