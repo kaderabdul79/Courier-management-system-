a9f04f68783b0179a85bd571ee50c78b9c077e58
kaderabdul <kaderabdul7980@gmail.com>
2022-03-02 18:16:21 +0600

    get form data and file, for file installed express-uploader pkg which seperated data

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const admin = require("firebase-admin");
 const { MongoClient } = require('mongodb');
+const fileupload = require('express-fileupload')
 const port = 5000
 
 // use middleware and parse data
 app.use(cors());
 app.use(express.json());
+app.use(fileupload());
+
+// apartment-firebase-adminsdk.json
+
+const serviceAccount = require('./apartment-firebase-adminsdk.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 const uri = "mongodb+srv://apartment:sai1eFqglF3FEUoL@cluster0.vxcvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 // const uri = `mongodb+srv://apartment:sai1eFqglF3FEUoL@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+      const token = req.headers.authorization.split(' ')[1];
+
+      try {
+          const decodedUser = await admin.auth().verifyIdToken(token);
+          req.decodedEmail = decodedUser.email;
+      }
+      catch {
+
+      }
+
+  }
+  next();
+}
+
 // 
 async function run() {
     try {
@@ -20,6 +47,14 @@ async function run() {
       const database = client.db("apartment");
       const scheduleCollection = database.collection("schedule");
       const usersCollection = database.collection("user");
+      const contactInfoCollection = database.collection("contactInfo");
+      const apartmentInfoCollection = database.collection("apartmentInfo");
+
+      app.post('/apartments', async (req, res) => {
+        const apartment = req.body;
+        const image = req.files;
+        console.log(apartment,image);
+    })
 
     // collect data from body and create post api to insert data for schedule
     app.post('/schedules', async (req, res) => {
@@ -33,7 +68,7 @@ async function run() {
     app.get('/schedules', async (req, res) => {
         const email = req.query.email;
         const query = {email: email}
-        console.log(query)
+        // console.log(query)
         const cursor = scheduleCollection.find(query);
         const schedules = await cursor.toArray();
         res.json(schedules);
@@ -58,14 +93,44 @@ async function run() {
     });
 
     // add role when admin will make someone admin
-    app.put('/users/admin', async (req, res) => {
+    app.put('/users/admin', verifyToken, async (req, res) => {
       const user = req.body;
-      const filter = { email: user.email };
-      const updateDoc = { $set: {role: 'admin'} };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.json(result);
+      // console.log(req.headers.authorization)
+      const requester = req.decodedEmail;
+      if (requester) {
+          const requesterAccount = await usersCollection.findOne({ email: requester });
+          if (requesterAccount.role === 'admin') {
+              const filter = { email: user.email };
+              const updateDoc = { $set: { role: 'admin' } };
+              const result = await usersCollection.updateOne(filter, updateDoc);
+              res.json(result);
+          }
+      }
+      else {
+          res.status(403).json({ message: 'you do not have access to make admin' })
+      }
   });
-  
+
+  //     
+  app.get('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        let isAdmin = false;
+        if(user?.role === 'admin'){
+          isAdmin = true;
+        }
+        res.json({admin: isAdmin});
+  });
+
+  // contacts Info post
+  app.post('/contacts', async (req, res) => {
+    const contacts = req.body;
+    const result = await contactInfoCollection.insertOne(contacts);
+    console.log(result);
+    res.json(result)
+})
+
     } finally {
     //   await client.close();
     }
